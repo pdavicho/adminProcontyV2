d867271371a14ac0c8faadabe3427ee92baa3a29
Pablo David Minango Negrete <42147807+pdavicho@users.noreply.github.com>
2023-07-13 16:06:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,18 @@ if menuPrincipal == 'Información':
     st.title("Pasos a Producción")
     st.write("""Seleccionar en el Menu Principal, una de las siguientes opciones:""")
     st.subheader(' 1. Junos ')
-    st.markdown('**:red[Archivo CSV - JUNOS:]**')
+    st.markdown('**Archivo CSV - JUNOS:**')
     st.write('Permite generar el archivo .csv para cargar las IPs maliciosas en el FW Atlas')
-    st.markdown('**:red[Archivo CSV JUNOS - GRUPO:]**')
+    st.markdown('**Archivo CSV JUNOS - GRUPO**')
     st.write('Permite generar el archivo .csv para cargar las IPs maliciosas en el FW Atlas, con la opción de colocar el grupo al cual se desea cargar')
-    st.markdown('**:red[Comparación de IPs - JUNOS:]**')
+    st.markdown('**Comparación de IPs - JUNOS**')
     st.write('Permite comparar las nuevas IPs de Ingeniera con la base de datos del FW Atlas')
     
     st.subheader('2. Mops ')
     st.write('Permite generar MOPs genéricos para la gestión por parte de N2 o Proveedores')
-    st.markdown('**:red[MOP Gestión N2 - Generico]**')
-    st.markdown('**:red[MOP Proveedor - Generico]**')
-    st.markdown('**:red[MOP Preaprobadas]**')
+    st.markdown('**MOP Gestión N2 - Generico**')
+    st.markdown('**MOP Proveedor - Generico**')
+    st.markdown('**MOP Preaprobadas**')
         
 elif menuPrincipal == '1. Junos':
     #st.sidebar.text("JUNOS")
@@ -33,5 +33,5 @@ elif menuPrincipal == '2. Mops':
     st.title('[MOPs Gestión - Preaprobadas]')
     mainMops()
 
-st.sidebar.write('All rights reserved. Developed by **:blue[David Minango]**')
+st.sidebar.write('All rights reserved. Developed by **David Minango**')
         
